83702d98b84560285d0db4da893f271e6795280c
Jacob Miske <jacobmiske@gmail.com>
2020-02-19 11:17:56 -0500

    chore: learning order placed

--- src/pages/PlacedOrder.js
+++ src/pages/PlacedOrder.js
@@ -1,28 +1,43 @@
-// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
+// MIT License
 
-import React from 'react'
+import React, { useContext } from 'react'
 import { Grid, Header } from 'semantic-ui-react'
 import TopMenu from "../components/TopMenu";
+import AppContext from "../context/AppContext";
+import styled from "styled-components";
 
-function PlacedOrder() {
-    return (
-        <Grid rows={2} style={topBanner}>
-            <TopMenu/>
-            <Grid.Row>
-                <Grid.Column width={12} verticalAlign='middle' textAlign='center' style={headerStyle}>
-                    <Header as='h1' textAlign='center'>
-                      Order Complete
-                    </Header>
-                </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <p>
-                Hello world
-              </p>
-            </Grid.Row>
-        </Grid>
-    )
+function PlacedOrder(props) {
+  // const { cart } = useContext(AppContext);
+  // const listItems = items.map((number) =>
+  //   <li>{number}</li>
+  // );
+  const { user } = props;
+  // const { total } = props;
+
+  function getAtt(name) {
+    return user ? user[name] : ""
+  }
+
+  return (
+      <Grid rows={2} style={topBanner}>
+          <TopMenu/>
+          <Grid.Row>
+              <Grid.Column width={12} verticalAlign='middle' textAlign='center' style={headerStyle}>
+                  <Header as='h1' textAlign='center'>
+                    Order Placed
+                  </Header>
+              </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={7}>
+              <NormalText>{getAtt('given_name') + ' ' + getAtt('family_name')}
+                <br/>{getAtt('custom:street')}<br/>{getAtt('custom:city')}, {getAtt('custom:state')} {getAtt('custom:postcode')}
+                <br/>{getAtt('custom:country')}
+              </NormalText>
+            </Grid.Column>
+          </Grid.Row>
+      </Grid>
+  )
 }
 
 export default PlacedOrder
@@ -38,3 +53,7 @@ const topBanner = {
 const headerStyle = {
     marginTop: '5px'
 };
+
+const NormalText = styled.div`
+  font-size: 1em;
+`;
